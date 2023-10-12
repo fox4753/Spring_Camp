ae5ef2ba9c81dd2ba8d61bf10db5b9902bea6db1
fox4753 <fox4753@gmail.com>
2023-10-12 18:33:14 +0900

    3주차 과제 게시판 구현

---
+++ btcw02/src/main/java/com/btc/persistence/BoardDAO.java
@@ -0,0 +1,13 @@
+package com.btc.persistence;
+
+import java.util.List;
+
+import com.btc.domain.BoardVO;
+
+public interface BoardDAO {
+	public void create(BoardVO vo) throws Exception;
+	public BoardVO read(Integer bno) throws Exception;
+	public void update(BoardVO vo) throws Exception;
+	public void delete(Integer bno) throws Exception;
+	public List<BoardVO> listAll() throws Exception;
+}
